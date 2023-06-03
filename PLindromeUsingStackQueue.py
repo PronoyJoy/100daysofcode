3e93428836849230f4bfb19c0bb1216c9236af4d
PronoyJoy <pronoy.das.developer@gmail.com>
2023-06-03 12:59:35 +0600

    hck

---
+++ PLindromeUsingStackQueue.py
@@ -0,0 +1,8 @@
+
+string = "mom"
+
+stack_box = []
+queue_box = []
+
+def stack(string): 
+   for i
---
+++ hackerank 30 day/day 2.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+second_integer = int(input())
+double = float(input())
+string = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+second_integer)
+
+# Print the sum of the double variables on a new line.
+print(round((d+double),1))
+# Concatenate and print the String variables on a new line
+print(s+string)
+# The 's' variable above should be printed first.
